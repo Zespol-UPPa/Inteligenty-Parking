3dbff14a15338df7d65a7e06b6aec57d526124dd
Krystian Ziaja <krystianziaja02@gmail.com>
2026-01-07 17:29:31 +0100

    Charges, controlling payments

---
+++ payment-service/src/main/java/com/smartparking/payment_service/dto/IdResponse.java
@@ -0,0 +1,21 @@
+package com.smartparking.payment_service.dto;
+
+public class IdResponse {
+    private Long id;
+
+    public IdResponse() {
+    }
+
+    public IdResponse(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
